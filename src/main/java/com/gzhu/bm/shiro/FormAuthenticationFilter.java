c74423e55da2a342b25cd4d3655d0cb9cf959ae8
chunzhi01.yang <chunzhi01.yang>
2017-05-03 18:19:50 +0800

    去掉没用的filter

--- src/main/java/com/gzhu/bm/shiro/FormAuthenticationFilter.java
+++ src/main/java/com/gzhu/bm/shiro/FormAuthenticationFilter.java
@@ -1,106 +1,43 @@
 package com.gzhu.bm.shiro;
 
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.web.util.WebUtils;
+import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component; 
-@Component("formAuthenticationFilter")
+
+@Component("logined")
 public class FormAuthenticationFilter extends org.apache.shiro.web.filter.authc.FormAuthenticationFilter {
 
-	private final Logger log = LoggerFactory.getLogger(getClass());  
-
-	@Override
-	protected boolean onAccessDenied(ServletRequest request, ServletResponse response,Object mappedValue) throws Exception {
-		 
-		HttpSession session = ((HttpServletRequest)request).getSession();
-
-         //页面输入的验证码
-
-         String randomcode = request.getParameter("randomcode");
-         if(randomcode == null){
-        	 return true;
-         }
-         //从session中取出验证码
-
-         String validateCode = (String) session.getAttribute("validateCode");
-
-         if (!randomcode.equals(validateCode)) {
-
-                // randomCodeError表示验证码错误
-
-                request.setAttribute("shiroLoginFailure", "randomCodeError");
-
-                //拒绝访问，不再校验账号和密码
-
-                return true;
-
-         } 
-         return super.onAccessDenied(request, response, mappedValue); 
-  
-	}	
-
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
 
 	@Override
-    protected AuthenticationToken createToken(ServletRequest request, ServletResponse response) {
-        
-        String username = getUsername(request);
-        String password = getPassword(request);
-        boolean rememberMe = isRememberMe(request);
-        String host = request.getRemoteAddr(); 
-        UsernamePasswordToken token =  new UsernamePasswordToken(username, password.toCharArray(),rememberMe,host);
-        return token;
-    }
+	protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+		HttpServletResponse resp = (HttpServletResponse) response;
+		PrintWriter out = resp.getWriter();
 
- 
- 
- 
- 
-    /**
-     * 登录成功之后跳转URL
-     */
-    @Override
-    public String getSuccessUrl() {
-        return super.getSuccessUrl();
-    }
-    
-    @Override
-    protected void issueSuccessRedirect(ServletRequest request,
-            ServletResponse response) throws Exception {
-        WebUtils.issueRedirect(request, response, getSuccessUrl(), null, true);
-    }
+		Map<String, Object> m = new HashMap<String, Object>();
+		m.put("msg", "您没有足够的权限执行该操作");
+		resp.sendError(403);
+		out.println(m);
+		out.flush();
+		out.close();
+		return false;
+	}
 
-    /**
-     * 登录失败调用事件
-     */
-    @Override
-    protected boolean onLoginFailure(AuthenticationToken token,
-            AuthenticationException e, ServletRequest request, ServletResponse response) {
-        String className = e.getClass().getName(), message = "";
-        if (IncorrectCredentialsException.class.getName().equals(className)
-                || UnknownAccountException.class.getName().equals(className)){
-            message = "用户名或密码错误！";
-        }
-        else if (e.getMessage() != null && StringUtils.startsWith(e.getMessage(), "msg:")){
-            message = StringUtils.replace(e.getMessage(), "msg:", "");
-        }
-        else{
-            message = "系统异常，请稍后再试！";
-            log.error(e.getMessage(), e);
-        }
-        request.setAttribute(getFailureKeyAttribute(), className); 
-        return true;
-    }
+	@Override
+	public boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
+		Subject subject = getSubject(request, response);
+		return subject.getPrincipal() != null;
+	}
+
   
 } 
